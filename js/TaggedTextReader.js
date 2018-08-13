8dfc3f537d7a3da2dd702e85f2e2d93952c6f1be
hosscine <futagamirentaro@gmail.com>
2018-08-13 12:33:17 +0900

    xhrの為にローカルサーバーをセットアップ

--- js/TaggedTextReader.js
+++ js/TaggedTextReader.js
@@ -1,18 +1,21 @@
-const TAG_PATTERN = /\[(\S+)\]/
+const TAG_PATTERN = /\[(\S+)\]/ // []で囲われた空白以外の文字列
 const NEW_LINE_PATTERN = /\n/
 
 class TaggedTextReader {
   constructor(path) {
 
     this.data = {}
+    // this.readText(path)
+
+    let hog = this.readTextFile(path)
 
     console.log(this.data)
-
+    console.log(hog)
   }
 
   readText(path) {
-    let fs = new ActiveXObject("Scripting.FileSystemObject");
-    let file = fs.OpenTextFile(path);
+    let file = ""
+    let reader = new FileReader;
 
     let currentTag = ""
 
@@ -20,15 +23,35 @@ class TaggedTextReader {
       text = file.ReadLine();
       console.log(text)
 
-      if(TAG_PATTERN.test(text))
+      if (TAG_PATTERN.test(text)) {
+        if (currentTag !== "") alert("error")
         currentTag = TAG_PATTERN.exec(text)[1]
-      else if(NEW_LINE_PATTERN.test(text))
-       currentTag = ""
+      } else if (NEW_LINE_PATTERN.test(text))
+        currentTag = ""
       else {
         if (this.data[currentTag] === undefined) this.data[currentTag] += text
         else this.data[currentTag] = text
       }
     }
+  }
+
+  hoge(path) {
+    httpObj = new XMLHttpRequest();
+    httpObj.open('GET', fileName + "?" + (new Date()).getTime(), true);
+    // ?以降はキャッシュされたファイルではなく、毎回読み込むためのもの
+    httpObj.send(null);
+    httpObj.onreadystatechange = function() {
+      if ((httpObj.readyState == 4) && (httpObj.status == 200)) {
+        document.getElementById("text1").value = httpObj.responseText;
+      }
+    }
+  }
+
+  readTextFile(fileName) {
+    var reqFile = new XMLHttpRequest();
+    reqFile.open('GET', fileName, true);
+    reqFile.send(null);
+    return reqFile.responseText;
   }
 
 }
