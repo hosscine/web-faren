3238288a5b73b0a7544536102f602d2300b6da29
hosscine <futagamirentaro@gmail.com>
2018-08-13 15:42:31 +0900

    formatTaggedTextを追加

--- js/TaggedTextReader.js
+++ js/TaggedTextReader.js
@@ -3,65 +3,41 @@ const NEW_LINE_PATTERN = /\n/
 
 class TaggedTextReader {
   constructor(path) {
-
     this.data = {}
-    // this.readText(path)
+    this.text
 
-    let hog = this.readTextFile(path)
-    this.readTextFile2(path)
+    this.readFile(path)
   }
 
-  readText(path) {
-    let file = ""
-    let reader = new FileReader;
-
-    let currentTag = ""
-
-    while (!file.AtEndOfStream) {
-      text = file.ReadLine();
-      console.log(text)
-
-      if (TAG_PATTERN.test(text)) {
-        if (currentTag !== "") alert("error")
-        currentTag = TAG_PATTERN.exec(text)[1]
-      } else if (NEW_LINE_PATTERN.test(text))
-        currentTag = ""
-      else {
-        if (this.data[currentTag] === undefined) this.data[currentTag] += text
-        else this.data[currentTag] = text
-      }
-    }
-  }
-
-  hoge(path) {
-    httpObj = new XMLHttpRequest();
-    httpObj.open('GET', fileName + "?" + (new Date()).getTime(), true);
-    // ?以降はキャッシュされたファイルではなく、毎回読み込むためのもの
-    httpObj.send(null);
-    httpObj.onreadystatechange = function() {
-      if ((httpObj.readyState == 4) && (httpObj.status == 200)) {
-        document.getElementById("text1").value = httpObj.responseText;
-      }
-    }
-  }
-
-  // readTextFile(path) {
-  //   var xhr = new XMLHttpRequest()
-  //   xhr.open("get", path, true)
-  //   xhr.onload = function() {
-  //     console.log(xhr.responseText)
-  //   }
-  //   xhr.send()
-  // }
-
-  readTextFile(path) {
-    let handleFileLoad = function(event){
+  readFile(path) {
+    let handleFileLoad = function(event) {
       console.log(event.result)
     }
-    let queue = new createjs.LoadQueue(true);
-    queue.on("fileload", handleFileLoad, this);
-    queue.loadFile(path);
+    let queue = new createjs.LoadQueue(true)
+    queue.on("fileload", (event) => this.formatTaggedText(event.result))
+    queue.loadFile(path)
     queue.load()
+  }
+
+  formatTaggedText(rawtext) {
+    this.text = rawtext.split(NEW_LINE_PATTERN)
+
+    let currentTag = ""
+    let contentsStart = 0
+
+    for (let i in this.text) {
+      let text = this.text[i]
+
+      if (TAG_PATTERN.test(text)) { // タグかどうか
+        if (currentTag !== "") alert(path + " " + i + "行目に不正なタグがあります")
+        currentTag = TAG_PATTERN.exec(text)[1]
+        contentsStart = parseInt(i) + 1
+      } else if (text === "") { // 改行かどうか
+        this.data[currentTag] = this.text.slice(contentsStart, parseInt(i)) // コンテンツ登録
+        currentTag = ""
+      }
+
+    }
   }
 
 }
