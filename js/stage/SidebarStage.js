1ba42f23ba2d779766b82cd31574af7fca236548
hosscine <futagamirentaro@gmail.com>
2018-09-11 03:54:38 +0900

    サイドバーをステージに管理させることにした

---
+++ js/stage/SidebarStage.js
@@ -0,0 +1,49 @@
+class SidebarStage extends createjs.Stage {
+  constructor(canvas) {
+    super(canvas)
+  }
+
+  displayScenario(scenarioData) {
+    this.scenarioInfo = this.addChild(new createjs.Container())
+
+    let background = this.scenarioInfo.addChild(new createjs.Shape())
+    background.graphics.beginFill("darkblue").drawRect(0, 0, SIDEBAR_WIDTH, 2000)
+
+    let name = this.scenarioInfo.addChild(new createjs.Text(scenarioData.Name, "18px arial", "white"))
+
+    let explanation = this.scenarioInfo.addChild(new createjs.Text("", "15px arial", "white"))
+    explanation.lineWidth = 200
+    explanation.y += 30
+    setWrapText(explanation, scenarioData.Explanation.join("\n"))
+  }
+
+}
+
+/**
+ * @description
+ * `Text`クラスのインスタンス`textInstance`の`text`プロパティに、
+ * `textInstance`の幅をはみ出さないように、自動で折り返した`text`を格納する。
+ * @param {Text} textInstance 自動で折り返した`text`を格納する`Text`クラスのインスタンス
+ * @param {string} text `textInstance.text`に格納する自動で折り返す文字列
+ */
+function setWrapText(textInstance, text) {
+  var initWidth = textInstance.lineWidth
+  var textArray = text.split('')
+  var i = -1
+  var prevText = ''
+  var lines = []
+
+  textInstance.text = ''
+
+  while (textArray[++i]) {
+    textInstance.text += textArray[i]
+
+    if (textInstance.getMeasuredWidth() > initWidth) {
+      lines.push(prevText)
+      textInstance.text = textArray[i]
+    }
+    prevText = textInstance.text
+  }
+
+  textInstance.text = lines.join('\n')
+}
