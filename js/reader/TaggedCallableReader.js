10e4fdbbc81dfdb09f4d0ac8dcb984ffd5a5f0b8
hosscine <futagamirentaro@gmail.com>
2018-09-22 18:04:32 +0900

    CallAbleを読み込むリーダーを追加

---
+++ js/reader/TaggedCallableReader.js
@@ -0,0 +1,35 @@
+const CALL_PATTERN = /Call(\d+)/
+const CALL_INIT_PATTERN = /Init(\d+)/
+
+class TaggedCallableReader extends TaggedTextReader {
+  constructor(path) {
+    super(path)
+
+    this.employable = []
+    this.initialEmployable = []
+  }
+
+  parseCallable() {
+    for (let tag in this.data) {
+      if (CALL_PATTERN.test(tag)) this.parseCall(tag, this.data[tag])
+      else if (CALL_INIT_PATTERN.test(tag)) this.parseInit(tag, this.data[tag])
+      else Error("Callableに不正なタグ", tag, "が使用されています。")
+    }
+  }
+
+  parseCall(tag, contents) {
+    let type = parseInt(CALL_PATTERN.exec(tag)[1])
+    if (this.employable.length !== type) Error("CallableのCallの順番がおかしいです。")
+    this.employable.push(contents)
+  }
+
+  parseInit(tag, contents) {
+    let type = parseInt(CALL_INIT_PATTERN.exec(tag)[1])
+    if (this.initialEmployable.length !== type) Error("CallableのInitの順番がおかしいです。")
+    this.initialEmployable.push(contents)
+  }
+
+  delegateLoadComplete() {
+    this.parseCallable()
+  }
+}
--- js/stage/PreloadAssets.js
+++ js/stage/PreloadAssets.js
@@ -7,10 +7,6 @@ const UNIT_PATTERN = /unit\d+/
 const FACE_PATTERN = /face\d+/
 const FLAG_PATTERN = /flag\d+/
 
-const STRATEGY_MAP_PATH = "assets/Default/Picture/FieldMap.png"
-const AREADATA_PATH = "assets/Default/Data/AreaData"
-const CHARACTERDATA_PATH = IMAGE_DIR + "CharacterData"
-
 function preloadAssets() {
   console.time("preload")
 
@@ -23,6 +19,10 @@ function preloadAssets() {
     {
       id: "areadata",
       src: DATA_DIR + "AreaData"
+    },
+    {
+      id: "callable",
+      src: DATA_DIR + "CallAble"
     }
   ]
 
@@ -31,7 +31,9 @@ function preloadAssets() {
       assets.charadata = new CharacterDataReader(event.result)
       assets.imageManifest = assets.imageManifest.concat(assets.charadata.imageManifest)
       setupMasters("charadata")
-    } else if (event.item.id === "areadata") assets.areaData = new AreaDataReader(event.result)
+    }
+    else if (event.item.id === "areadata") assets.areaData = new AreaDataReader(event.result)
+    else if (event.item.id === "callable") assets.callable = new TaggedCallableReader(event.result)
   }
 
   // スクリプト読み込み完了時に画像読み込みを開始
@@ -63,12 +65,12 @@ function preloadAssets() {
     let i = event.item
     if (i.id === "map") assets.strategyMap = new createjs.Bitmap(event.result)
     else if (i.id === "neutralFlag") assets.neutralFlag = new AlphalizeBitmap(event.result)
-    else if (UNIT_PATTERN.test(i.id)) 
-      for(let id of i.unitID) assets.charadata.characters[id].unitImage = new AlphalizeBitmap(event.result)
+    else if (UNIT_PATTERN.test(i.id))
+      for (let id of i.unitID) assets.charadata.characters[id].unitImage = new AlphalizeBitmap(event.result)
     else if (FACE_PATTERN.test(i.id))
-      for(let id of i.unitID) assets.charadata.characters[id].faceImage = new AlphalizeBitmap(event.result)
+      for (let id of i.unitID) assets.charadata.characters[id].faceImage = new AlphalizeBitmap(event.result)
     else if (FLAG_PATTERN.test(i.id))
-      for(let id of i.unitID) assets.charadata.characters[id].flagImage = new AlphalizeBitmap(event.result)
+      for (let id of i.unitID) assets.charadata.characters[id].flagImage = new AlphalizeBitmap(event.result)
   }
 
   // 画像読み込み完了時にユニットのインスタンスを作成
@@ -97,8 +99,5 @@ function setupMasters(readyKey) {
       master.setup(assets)
       stage.mastersDic[master.id] = master
     }
-    // stage.displayMasters()
   }
-
-  // if (assets.readyFlag.scenario & assets.readyFlag.charadata & assets.readyFlag.image)
 }
