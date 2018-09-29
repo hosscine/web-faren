f4a963b42077c31576cfd070003933a860e9ca8f
hosscine <futagamirentaro@gmail.com>
2018-09-29 22:07:17 +0900

    縁取りテキストのクラスを追加

---
+++ js/UI/OutlineText.js
@@ -0,0 +1,24 @@
+class OutlineText extends createjs.Container {
+  constructor(text, font = "15px arial") {
+    super()
+
+    let frontText = new createjs.Text(text, font, "white")
+    let outlineText = frontText.clone()
+    outlineText.color = "black"
+    outlineText.outline = 2
+
+    this.setup()
+  }
+
+  set text(text) {
+    for (let child of this.children) child.text = value
+  }
+  get text() {
+    return (this.children[0].text)
+  }
+
+  set textAlign(value) {
+    for (let child of this.children) child.textAlign = value
+  }
+
+}
