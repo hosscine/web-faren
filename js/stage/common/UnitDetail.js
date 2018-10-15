ebe19de2a9dc45abcb8b93cc050d85e073db6ad4
hosscine <futagamirentaro@gmail.com>
2018-10-15 19:15:55 +0900

    さらにリファクタリング

--- js/stage/common/UnitDetail.js
+++ js/stage/common/UnitDetail.js
@@ -60,7 +60,7 @@ class UnitDetail extends createjs.Container {
     this.data.resist.text = Object.keys(unit.resist).reduce((a, x) =>
       a + (unit.resist[x] === 0 ? "" : STRING_MAP_RESIST[x] + "攻撃に" + effect[parseInt(unit.resist[x])] + "\n"), "")
 
-    this.detailFoot.y = this.detailFoot.offset + this.data.resist.getMeasuredHeight() + 5
+    this.footerContainer.y = this.footerContainer.offset + this.data.resist.getMeasuredHeight() + 5
     this.data.killStats.text = this._format3digits(unit.killStats)
     this.data.cost.text = unit.isMaster || unit.characterType.named > 0 ? this._format3digits(unit.cost) : this._format3digits(0)
 
@@ -68,172 +68,173 @@ class UnitDetail extends createjs.Container {
   }
 
   setup() {
-    let unitDetailContainer = this.addChild(new createjs.Container())
-    this.container = unitDetailContainer
-    unitDetailContainer.visible = false
+    let container = this.addChild(new createjs.Container())
+    this.container = container
+    container.visible = false
 
-    let background = unitDetailContainer.addChild(new createjs.Shape())
+    let background = container.addChild(new createjs.Shape())
     background.graphics.beginFill("darkblue").drawRect(0, 0, SIDEBAR_WIDTH, 750)
     background.graphics.beginStroke("white").drawRect(5, 5, SIDEBAR_WIDTH - 10, 750)
 
-    let detailContentsContainer = unitDetailContainer.addChild(new createjs.Container())
-    this.subContainer = detailContentsContainer
+    let subContainer = container.addChild(new createjs.Container())
+    this.subContainer = subContainer
 
     let contentY = 0
-    let name = detailContentsContainer.addChild(new createjs.Text("ファイアジャイアント", "15px arial", "white"))
+    let name = subContainer.addChild(new createjs.Text("ファイアジャイアント", "15px arial", "white"))
     name.x = 45
     name.y = contentY += 8
 
-    let species = detailContentsContainer.addChild(new createjs.Text("人間系", "14px arial", "white"))
+    let species = subContainer.addChild(new createjs.Text("人間系", "14px arial", "white"))
     species.x = 50
     species.y = contentY += 20
 
-    let named = detailContentsContainer.addChild(new createjs.Text("（人材）", "14px arial", "white"))
+    let named = subContainer.addChild(new createjs.Text("（人材）", "14px arial", "white"))
     named.x = 130
     named.y = contentY
 
-    let rank = detailContentsContainer.addChild(new createjs.Text("Rank S", "14px arial", "white"))
+    let rank = subContainer.addChild(new createjs.Text("Rank S", "14px arial", "white"))
     rank.x = 50
     rank.y = contentY += 15
 
-    let experience = detailContentsContainer.addChild(new createjs.Text("Exp 100", "14px arial", "white"))
+    let experience = subContainer.addChild(new createjs.Text("Exp 100", "14px arial", "white"))
     experience.x = 120
     experience.y = contentY
 
     const contentX = 40
     const contentX2 = 105
-    let lHP = detailContentsContainer.addChild(new createjs.Text("HP", "14px arial", "white"))
-    let HP = detailContentsContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
+    let lHP = subContainer.addChild(new createjs.Text("HP", "14px arial", "white"))
+    let HP = subContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
     lHP.x = contentX
     HP.x = contentX2
     lHP.y = HP.y = contentY += 20
 
-    let lMP = detailContentsContainer.addChild(new createjs.Text("MP", "14px arial", "white"))
-    let MP = detailContentsContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
+    let lMP = subContainer.addChild(new createjs.Text("MP", "14px arial", "white"))
+    let MP = subContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
     lMP.x = contentX
     MP.x = contentX2
     lMP.y = MP.y = contentY += 15
 
-    let lphysicalStrength = detailContentsContainer.addChild(new createjs.Text("攻撃力", "14px arial", "white"))
-    let physicalStrength = detailContentsContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
+    let lphysicalStrength = subContainer.addChild(new createjs.Text("攻撃力", "14px arial", "white"))
+    let physicalStrength = subContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
     lphysicalStrength.x = contentX
     physicalStrength.x = contentX2
     lphysicalStrength.y = physicalStrength.y = contentY += 15
 
-    let lphysicalResistance = detailContentsContainer.addChild(new createjs.Text("防御力", "14px arial", "white"))
-    let physicalResistance = detailContentsContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
+    let lphysicalResistance = subContainer.addChild(new createjs.Text("防御力", "14px arial", "white"))
+    let physicalResistance = subContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
     lphysicalResistance.x = contentX
     physicalResistance.x = contentX2
     lphysicalResistance.y = physicalResistance.y = contentY += 15
 
-    let ltechnique = detailContentsContainer.addChild(new createjs.Text("技量", "14px arial", "white"))
-    let technique = detailContentsContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
+    let ltechnique = subContainer.addChild(new createjs.Text("技量", "14px arial", "white"))
+    let technique = subContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
     ltechnique.x = contentX
     technique.x = contentX2
     ltechnique.y = technique.y = contentY += 15
 
-    let lagility = detailContentsContainer.addChild(new createjs.Text("素早さ", "14px arial", "white"))
-    let agility = detailContentsContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
+    let lagility = subContainer.addChild(new createjs.Text("素早さ", "14px arial", "white"))
+    let agility = subContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
     lagility.x = contentX
     agility.x = contentX2
     lagility.y = agility.y = contentY += 15
 
-    let lmagicalStrength = detailContentsContainer.addChild(new createjs.Text("魔力", "14px arial", "white"))
-    let magicalStrength = detailContentsContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
+    let lmagicalStrength = subContainer.addChild(new createjs.Text("魔力", "14px arial", "white"))
+    let magicalStrength = subContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
     lmagicalStrength.x = contentX
     magicalStrength.x = contentX2
     lmagicalStrength.y = magicalStrength.y = contentY += 15
 
-    let lmagicalResistance = detailContentsContainer.addChild(new createjs.Text("抵抗力", "14px arial", "white"))
-    let magicalResistance = detailContentsContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
+    let lmagicalResistance = subContainer.addChild(new createjs.Text("抵抗力", "14px arial", "white"))
+    let magicalResistance = subContainer.addChild(new createjs.Text("100/100", "14px Courier New", "white"))
     lmagicalResistance.x = contentX
     magicalResistance.x = contentX2
     lmagicalResistance.y = magicalResistance.y = contentY += 15
 
-    let lMPrecover = detailContentsContainer.addChild(new createjs.Text("HP再生", "14px arial", "white"))
-    let MPrecover = detailContentsContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
+    let lMPrecover = subContainer.addChild(new createjs.Text("HP再生", "14px arial", "white"))
+    let MPrecover = subContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
     lMPrecover.x = contentX
     MPrecover.x = contentX2
     lMPrecover.y = MPrecover.y = contentY += 15
 
-    let lHPrecover = detailContentsContainer.addChild(new createjs.Text("MP再生", "14px arial", "white"))
-    let HPrecover = detailContentsContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
+    let lHPrecover = subContainer.addChild(new createjs.Text("MP再生", "14px arial", "white"))
+    let HPrecover = subContainer.addChild(new createjs.Text(" 10/ 10", "14px Courier New", "white"))
     lHPrecover.x = contentX
     HPrecover.x = contentX2
     lHPrecover.y = HPrecover.y = contentY += 15
 
     const magicMargine = 22
-    let magicFire = detailContentsContainer.addChild(new createjs.Text("火\nＳ", "14px arial", "white"))
+    let magicFire = subContainer.addChild(new createjs.Text("火\nＳ", "14px arial", "white"))
     magicFire.x = 38
     magicFire.y = contentY += 25
 
-    let magicAqua = detailContentsContainer.addChild(new createjs.Text("水\n✖", "14px arial", "white"))
+    let magicAqua = subContainer.addChild(new createjs.Text("水\n✖", "14px arial", "white"))
     magicAqua.x = magicFire.x + magicMargine
     magicAqua.y = contentY
 
-    let magicWind = detailContentsContainer.addChild(new createjs.Text("風\n✖", "14px arial", "white"))
+    let magicWind = subContainer.addChild(new createjs.Text("風\n✖", "14px arial", "white"))
     magicWind.x = magicAqua.x + magicMargine
     magicWind.y = contentY
 
-    let magicEarth = detailContentsContainer.addChild(new createjs.Text("土\n✖", "14px arial", "white"))
+    let magicEarth = subContainer.addChild(new createjs.Text("土\n✖", "14px arial", "white"))
     magicEarth.x = magicWind.x + magicMargine
     magicEarth.y = contentY
 
-    let magicLight = detailContentsContainer.addChild(new createjs.Text("光\n✖", "14px arial", "white"))
+    let magicLight = subContainer.addChild(new createjs.Text("光\n✖", "14px arial", "white"))
     magicLight.x = magicEarth.x + magicMargine
     magicLight.y = contentY
 
-    let magicDark = detailContentsContainer.addChild(new createjs.Text("闇\n✖", "14px arial", "white"))
+    let magicDark = subContainer.addChild(new createjs.Text("闇\n✖", "14px arial", "white"))
     magicDark.x = magicLight.x + magicMargine
     magicDark.y = contentY
 
-    let attackTimes = detailContentsContainer.addChild(new createjs.Text("攻撃 × 3", "14px arial", "white"))
+    let attackTimes = subContainer.addChild(new createjs.Text("攻撃 × 3", "14px arial", "white"))
     attackTimes.x = 30
     attackTimes.y = contentY += 45
 
-    let attackTypes = detailContentsContainer.addChild(new createjs.Text("（通常 神聖 毒）", "14px arial", "white"))
+    let attackTypes = subContainer.addChild(new createjs.Text("（通常 神聖 毒）", "14px arial", "white"))
     attackTypes.x = 20
     attackTypes.y = contentY += 15
 
-    let uniqueSkil = detailContentsContainer.addChild(new createjs.Text("弓矢 × 3", "14px arial", "white"))
+    let uniqueSkil = subContainer.addChild(new createjs.Text("弓矢 × 3", "14px arial", "white"))
     uniqueSkil.x = 30
     uniqueSkil.y = contentY += 15
 
-    let moveType = detailContentsContainer.addChild(new createjs.Text("移動タイプ：飛行タイプ", "14px arial", "white"))
+    let moveType = subContainer.addChild(new createjs.Text("移動タイプ：飛行タイプ", "14px arial", "white"))
     moveType.x = 25
     moveType.y = contentY += 30
 
-    let moveRange = detailContentsContainer.addChild(new createjs.Text("　　移動力：10", "14px arial", "white"))
+    let moveRange = subContainer.addChild(new createjs.Text("　　移動力：10", "14px arial", "white"))
     moveRange.x = 25
     moveRange.y = contentY += 15
 
-    let resist = detailContentsContainer.addChild(new createjs.Text("石化攻撃に強い\n即死攻撃に強い", "14px arial", "white"))
+    let resist = subContainer.addChild(new createjs.Text("石化攻撃に強い\n即死攻撃に強い", "14px arial", "white"))
     resist.x = 50
     resist.y = contentY += 30
 
-    let detailFoot = detailContentsContainer.addChild(new createjs.Container())
-    this.detailFoot = detailFoot
-    detailFoot.offset = contentY
-    detailFoot.y = contentY += resist.getMeasuredHeight() + 17
+    // resistの長さが可変なのでfooterContainerの位置は調節される
+    let footerContainer = subContainer.addChild(new createjs.Container())
+    this.footerContainer = footerContainer
+    footerContainer.offset = contentY
+    footerContainer.y = contentY += resist.getMeasuredHeight() + 17
     contentY = 0
 
-    let lkillStats = detailFoot.addChild(new createjs.Text("撃破数", "14px arial", "white"))
-    let killStats = detailFoot.addChild(new createjs.Text("100", "14px Courier New", "white"))
+    let lkillStats = footerContainer.addChild(new createjs.Text("撃破数", "14px arial", "white"))
+    let killStats = footerContainer.addChild(new createjs.Text("100", "14px Courier New", "white"))
     lkillStats.x = 45
     killStats.x = 125
 
-    let lcost = detailFoot.addChild(new createjs.Text("人件費", "14px arial", "white"))
-    let cost = detailFoot.addChild(new createjs.Text("  5", "14px Courier New", "white"))
+    let lcost = footerContainer.addChild(new createjs.Text("人件費", "14px arial", "white"))
+    let cost = footerContainer.addChild(new createjs.Text("  5", "14px Courier New", "white"))
     lcost.x = 45
     cost.x = 125
     lcost.y = cost.y = contentY += 15
 
-    let button = detailFoot.addChild(new Button("閉じる", 100, 20))
+    let button = footerContainer.addChild(new Button("閉じる", 100, 20))
     button.regX = this.getBounds().width / 4
     button.x = 100
     button.y = contentY += 40
 
-    unitDetailContainer.on("click", () => this.undisplay())
+    container.on("click", () => this.undisplay())
     button.on("click", () => this.undisplay())
 
     this.data = {
