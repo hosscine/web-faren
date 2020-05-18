82022a516dccddd604bf6698f84c2a285c888e33
hosscine <futagamirentaro@gmail.com>
2020-05-18 19:25:30 +0900

    add readEscapeZoneMap

--- js/reader/BattleMapReader.js
+++ js/reader/BattleMapReader.js
@@ -1,4 +1,6 @@
-const MAP_SIZE = 32
+const BATTLE_MAP_SIZE = 32
+const MAP_MAX_ADJACENCY = 8
+const AREA_PLACEABLE_UNITS = 20
 
 class BattleMapReader extends BinaryReader {
   constructor(data) {
@@ -18,25 +20,36 @@ class BattleMapReader extends BinaryReader {
     this.readFrontChipMap()
     this.readStrategyChipMap()
     this.readHeightMap()
+    this.readEscapeZoneMap()
   }
   
   readMapLine(i) {
-    return(this.getBitArray10(i * MAP_SIZE, (i + 1) * MAP_SIZE - 1))
+    return(this.getBitArray10(i * BATTLE_MAP_SIZE, (i + 1) * BATTLE_MAP_SIZE - 1))
+  }
+  readZoneMapLine(i) {
+    let mixedLine = this.getBitArray10(i * AREA_PLACEABLE_UNITS * 2, (i + 1) * AREA_PLACEABLE_UNITS * 2 - 1)
+    let zones = []
+    for (let j = 0; j < AREA_PLACEABLE_UNITS; j++) zones.push({x: mixedLine[j * 2], y: mixedLine[j * 2 + 1]})
+    return(zones)
   }
 
   readBackgroundChipMap() {
-    for (let i = 0; i < MAP_SIZE; i++) this.backgroundChipMap.push(this.readMapLine(i))
+    for (let i = 0; i < BATTLE_MAP_SIZE; i++) this.backgroundChipMap.push(this.readMapLine(i))
   }
   readFrontChipMap() {
     this.increseOffset10(32**2)
-    for (let i = 0; i < MAP_SIZE; i++) this.frontChipMap.push(this.readMapLine(i))
+    for (let i = 0; i < BATTLE_MAP_SIZE; i++) this.frontChipMap.push(this.readMapLine(i))
   }
   readStrategyChipMap() {
     this.increseOffset10(32**2)
-    for (let i = 0; i < MAP_SIZE; i++) this.strategyChipMap.push(this.readMapLine(i))
+    for (let i = 0; i < BATTLE_MAP_SIZE; i++) this.strategyChipMap.push(this.readMapLine(i))
   }
   readHeightMap() {
     this.increseOffset10(32**2)
-    for (let i = 0; i < MAP_SIZE; i++) this.heightMap.push(this.readMapLine(i))
+    for (let i = 0; i < BATTLE_MAP_SIZE; i++) this.heightMap.push(this.readMapLine(i))
+  }
+  readEscapeZoneMap() {
+    this.increseOffset10(32**2)
+    for (let i = 0; i < MAP_MAX_ADJACENCY; i ++) this.escapeZoneMap.push(this.readZoneMapLine(i))
   }
 }
